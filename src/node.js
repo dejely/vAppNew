14fc74e2907ff17223f4d7aa8df1abeade158714
Dejel Cyrus De Asis <38832544+dejely@users.noreply.github.com>
2025-04-07 21:03:42 +0800

    Add files via upload

--- src/node.js
+++ src/node.js
@@ -2,7 +2,7 @@ import contractABI from './contractABI.json';
 
 const contractAddress = "0xEf8EA51aF4cb4e441d56Fc7938a1D4D303F6Ec1a"
 
-let web3 = new Web3(window.ethereum);
+let web3 = new Web3(windows.ethereum);
 
 let contract = new web3.eth.Contract(contractABI, contractAddress);
 
